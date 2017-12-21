27087ac1c80ac06a13c00f181215aebff47e7e74
Nick Forster <nforster24@gmail.com>
2017-12-20 23:09:30 -0800

    bare minimum fulfilled

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -11,50 +11,23 @@ class App extends React.Component {
       list: groceryList
     }
   }
- 
+
   handleClick(groceryObj) {
-  	// loop through this.state.list.groceryList
-  	// if any object's description property matches
-  	// increment its count
-  	// return
-  	//debugger;
   	var flag = true; 
   	var grocList = this.state.list.groceryList;
-  	debugger;
+  
   	for (var i = 0; i < grocList.length; i++) {
-  		var index = i;
-  	  if (grocList[i].description === groceryObj.description) {
-  	  	//debugger;
-  	  	// update the quantity property of grocList[i]
-  	  	this.setState((prevState) => {
-  	  	  var prevList = prevState.list.groceryList;
-  	  	  var matchedEntry = prevState.list.groceryList[index - 1];
-  	  	  console.log('matched entry is', matchedEntry)
-  	  	  console.log('previous list is', prevList)
-  	  	  var prevListClone = prevList.slice(0, prevList.length);
-  	  	  prevListClone.splice(index, 1);
-  	  	  var newQuantity = Number(matchedEntry.quantity) + Number(groceryObj.quantity);
-  	  	  var newNode = matchedEntry;
-  	  	  newNode.quantity = newQuantity;
-  	  	  // insert new node into prevListClone
-  	  	  var backHalf = prevListClone.slice(index-2, prevListClone.length)
-  	  	  prevListClone = prevListClone.slice(0, index - 2).concat(newNode);
-  	  	  prevListClone = prevListClone.concat(backHalf);
-  	  	  {list: prevListClone}
-  	  	  console.log(prevListClone)
-  	  	  //console.log(newQuantity)
-  	  	  console.log(prevListClone)
-
-  	  	});
-
-  	  	console.log('made it here')
-  	  //this.state.list.groceryList[i].quantity = this.state.list.groceryList[i].quantity + groceryObj.quantity;
+  	  if (grocList[i].description.toLowerCase() === groceryObj.description.toLowerCase()) {
+        var index = i;
+  	  	this.setState(() => {
+          this.state.list.groceryList[index].quantity = Number(this.state.list.groceryList[index].quantity) + Number(groceryObj.quantity);   
+          {list: this.state.groceryList}
+  	  	
+        });
   	  	flag = false;
   	  }
   	}
-  	// increment count
-  	// if (this.state.list..indexOf(groceryObj.description))
-  	console.log(this.state.list.groceryList)
+
   	if (flag) {
 	  	this.setState((prevState) => {
 	  		var prevArr = prevState.list.groceryList;
@@ -91,3 +64,28 @@ ReactDOM.render(<App />, document.getElementById('app'));
   	  	// 	{list: newList}
 
   	  	// });
+
+
+
+
+          // var prevList = grocList.slice();
+          // console.log('prevList is', prevList);
+          // var matchedEntry = prevState.list.groceryList[index - 1];
+          // console.log('matchedEntry is', matchedEntry)
+          // // console.log('matched entry is', matchedEntry)
+          // // console.log('previous list is', prevList)
+          // var prevListClone = prevList.slice(0, prevList.length);
+          // prevListClone.splice(index, 1);
+          // console.log('prevListClone after splice is', prevListClone);
+          // var newQuantity = Number(matchedEntry.quantity) + Number(groceryObj.quantity);
+          // var newNode = matchedEntry;
+          // newNode.quantity = newQuantity;
+          // console.log('newNode is', newNode);
+          // //newNode.description = groceryObj.description
+          // // insert new node into prevListClone
+          // var backHalf = prevListClone.slice(index-2, prevListClone.length)
+          // console.log('backHalf is', backHalf);
+          // prevListClone = prevListClone.slice(0, index - 2).concat(newNode);
+          // prevListClone = prevListClone.concat(backHalf);
+          // console.log('Final Product is', prevListClone)
+          // {list: prevListClone}
